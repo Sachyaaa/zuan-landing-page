fc4eabb904a58db42ac73da2e3d37b5af5753bb1
Sachyaaa <84377689+Sachyaaa@users.noreply.github.com>
2022-01-27 23:11:54 +0530

    some chamges

--- src/Cource.js
+++ src/Cource.js
@@ -14,19 +14,23 @@ export default function Cource(props) {
             <span className='span'>
             <img style={{display: props.bookmark}} className='bookmark' src={Bookmark} alt="" />
                 <img src={Date} alt="" />
-                <div >{props.date} | {props.from}-{props.to}</div>
+                <span >{props.date} | {props.from}-{props.to}</span>
             </span>
         </div>
         <div className="cource-lower">
+            <div>
             <h3>{props.name}</h3>
             <small>Oral Surgery</small>
-            {props.completed &&<div className="progress">
+            </div>
+            <div className="progress">
                 <span>Course Completion </span>
                 <div className="progress-bar">
-                    <span className="material"></span>
+                    <span className="material" style={{
+                        width: props.completed
+                    }}></span>
                 </div>
-            </div> }
-            <div className="rating">
+            </div> 
+            {props.rating &&<div className="rating">
                 <div>You Rated:</div>
                 <div>
                     <img src={Star} alt="" />
@@ -35,7 +39,7 @@ export default function Cource(props) {
                     <img src={Star} alt="" />
                     <img src={Star} alt="" />
                 </div>
-            </div>
+            </div>}
         </div>
         
     </div >;
--- src/Cources.js
+++ src/Cources.js
@@ -11,14 +11,14 @@ export default function Cources() {
       <RightBtns />
     </div>
     <div className="cources-list">
-      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' progr='true' bookmark='none' completed='50%'/>
-      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' progr='false' bookmark='block'/>
-      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' progr='true' bookmark='block'/>
-      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' progr='true' bookmark='none'/>
-      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' bookmark='block'/>
-      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' progr='true' bookmark='none'/>
-      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' bookmark='none'/>
-      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' bookmark='block'/>
+      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' rating='true' bookmark='none' completed='50%'/>
+      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography'  bookmark='block' completed='90%'/>
+      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' rating='true' bookmark='block'completed='80%'/>
+      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' rating='true' bookmark='none'completed='10%'/>
+      
+      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography'  bookmark='none'completed='100%'/>
+      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography'rating bookmark='none'completed='50%'/>
+      <Cource img={Teeth} date='6 Jan 2021' from='08:00pm' to='09:00pm' name='Master Class on Dental Photography' bookmark='block' completed='60%'/>
     </div>
   </div>;
 }
